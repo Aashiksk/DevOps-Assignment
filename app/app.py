5d53063eb3f6ebd684296610cea7247a01ec9c34
Aashik <your_github_email@example.com>
2026-07-10 15:30:10 +0530

    complete flask App

---
+++ app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>DevOps Technical Assignment</h1>
+    <p>Application is running successfully!</p>
+    """
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "healthy",
+        "message": "Application is running"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
